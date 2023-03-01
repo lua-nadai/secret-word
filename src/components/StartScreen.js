fcd39b856eb031da62e5414878203aefc41429e0
Luana Nadai <luana.nadai.santos@gmail.com>
2023-03-01 09:40:44 -0300

    Configuraçoes de CSS e informaçoes para a tela inicial

---
+++ src/components/StartScreen.js
@@ -0,0 +1,13 @@
+import './StartScreen.css'
+
+const StartScreen = () => {
+  return (
+    <div className='start'>
+      <h1>Secret Word</h1>
+      <p>Clique no botão abaixo para começar o jogo!</p>
+      <button>Iniciar</button>
+    </div>
+  )
+}
+
+export default StartScreen
