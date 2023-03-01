69dd04d97f5c13ce6937510b59213c49ea091fce
Luana Nadai <luana.nadai.santos@gmail.com>
2023-03-01 10:11:35 -0300

    Criado funcoes para setar os states inicais de word, category e letters. Componentes interligados por botoes

--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -1,11 +1,11 @@
 import './StartScreen.css'
 
-const StartScreen = () => {
+const StartScreen = ({ startGame }) => {
   return (
     <div className='start'>
       <h1>Secret Word</h1>
       <p>Clique no botão abaixo para começar o jogo!</p>
-      <button>Iniciar</button>
+      <button onClick={startGame}>Iniciar</button>
     </div>
   )
 }
