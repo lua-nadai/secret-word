67518bbd7e1eb05033593b51864466a6588b8972
Luana Nadai <luana.nadai.santos@gmail.com>
2023-03-01 18:22:46 -0300

    Estruturaçao Game component

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,9 +2,28 @@ import './Game.css'
 
 const Game = ({verifyLetter}) => {
   return (
-    <div>
-      <h1>Game</h1>
-      <button onClick={verifyLetter}>Finalizar jogo</button>
+    <div className="game">
+      <p className="point">Pontuação: 000</p>
+      <h1>Advinhe a palavra</h1>
+      <h3 className="tip">
+        Dica sobre a palavra: <span>DICA</span>
+      </h3>
+      <div className="wordContainer">
+        <span className="letter">A</span>
+        <span className="blankSquare"></span>
+      </div>
+      <div className="letterContainer">
+        <p>Tente advinhar uma letra da palavra:</p>
+        <form>
+          <input type="text" name='letter' maxLength={1} required />
+          <button>Jogar!</button>
+        </form>
+      </div>
+      <div className="wrongLettersContainer">
+        <p>Letras utilizadas:</p>
+        <span>A,</span>
+        <span>N,</span>
+      </div>
     </div>
   )
 }
