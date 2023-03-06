905759ccbef357f7ca34b782ac5ec22738cf64fc
Luana Nadai <luana.nadai.santos@gmail.com>
2023-03-06 12:53:08 -0300

    Correcao renderizacao tela inicial

--- src/index.js
+++ src/index.js
@@ -5,13 +5,13 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { HashRouter } from "react-router-dom";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <HashRouter>
       <App />
     </HashRouter>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
